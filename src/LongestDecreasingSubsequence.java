283c83e8f30fdee59cb27e3b330ba6b1e3163464
ahmad <abomoaz3375@gmail.com>
2022-04-06 15:07:24 +0300

    one

---
+++ src/LongestDecreasingSubsequence.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class LongestDecreasingSubsequence {
+    public static int LDS(int[] nums) {
+        int[] num2 = new int[nums.length];
+        Arrays.fill(num2, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] < nums[j] && num2[j] <= num2[j])
+                    num2[i] = num2[j] + 1;
+            }
+        }
+        return Arrays.stream(num2).max().getAsInt();
+    }
+
+    public static void main(String[] args) {
+        //int[] arr = {2, 4, 1, 7, 9};
+        //int[] arr = {15, 27, 14, 38, 63, 55, 46, 65, 85};
+        int[] arr = {50, 3, 10, 7, 40, 80};
+        System.out.println(LDS(arr));
+    }
+}
