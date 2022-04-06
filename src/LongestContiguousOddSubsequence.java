0dfd6e1dcadde2aa0c81ab9ae6b7c4eb68bcd931
ahmad <abomoaz3375@gmail.com>
2022-04-06 21:15:21 +0300

    one

--- src/LongestContiguousOddSubsequence.java
+++ src/LongestContiguousOddSubsequence.java
@@ -25,7 +25,10 @@ public class LongestContiguousOddSubsequence {
 
     public static void main(String[] args) {
         //int[] arr = {2, 4, 1, 7, 9, 5, 2, 7, 11, 17, 1};
-        int[] arr = {1, 2, 19, 5, 4, 7, 51, 23, 22, 13, 15, 36};
+        //int[] arr = {1, 2, 19, 5, 4, 7, 51, 23, 22, 13, 15, 36};
+        int[] arr = {12, 14, 16, 5, 7, 8};
+        int[] arr1 = {1, 2, 19, 5, 4, 7, 51, 23, 22, 13, 15, 36};
         System.out.println(LCOS(arr));
+        System.out.println(LCOS(arr1));
     }
 }
